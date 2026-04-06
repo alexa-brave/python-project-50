5bfe04b587fe8995a09e846da61e69dca1c92976
alexa-brave <merkel_795@mail.ru>
2026-04-06 15:51:54 +0300

    test: написала тесты для plain форматера

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import yaml
 from yaml import SafeLoader
 
-from gendiff.find_diff import build_diff
+from gendiff.find_diff import build_diff, generate_diff
 from gendiff.scripts.gendiff import reading_files
 from gendiff.visual import stylish
 
@@ -90,3 +90,15 @@ def test_diff_yaml_rec():
     result = read_file('expected_rec.txt')
     diff = build_diff(file1, file2)
     assert stylish(diff) == result
+
+
+def test_plain_format_json():
+    file1 = parser_test_json('file1_rec.json')
+    file2 = parser_test_json('file2_rec.json')
+    assert generate_diff(file1, file2) == read_file('expected_plain.txt')
+
+
+def test_plain_format_yaml():
+    file1 = parser_test_yaml('file1_rec.yaml')
+    file2 = parser_test_yaml('file2_rec.yaml')
+    assert generate_diff(file1, file2) == read_file('expected_plain.txt')
