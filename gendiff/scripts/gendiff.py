33c547ce6c54146d45570e41b2900690e5698efd
alexa-brave <merkel_795@mail.ru>
2026-04-18 19:06:44 +0300

    refactor: изменила точку входа, перенесла функцию reading_files в файл find_diff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,29 +1,8 @@
-import json
-
-import yaml
-from yaml import SafeLoader
-
 from ..files_parser import files_parser
 from ..find_diff import generate_diff
 
 
-def reading_files(args):
-    if args.first_file[-4:] == 'json':
-        with open(args.first_file) as f:
-            data1 = json.load(f)
-        with open(args.second_file) as f:
-            data2 = json.load(f)
-
-    elif args.first_file[-4:] == 'yaml' or args.first_file[-3:] == 'yml':
-        with open(args.first_file) as f:
-            data1 = yaml.load(f, Loader=SafeLoader)
-        with open(args.second_file) as f:
-            data2 = yaml.load(f, Loader=SafeLoader)
-    return data1, data2
-
-
 def main():
     args = files_parser()
-    data1, data2 = reading_files(args)
-    result = generate_diff(data1, data2, args.format)
+    result = generate_diff(args.first_file, args.second_file, args.format)
     return result
