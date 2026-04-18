f3034dfac3e728db79ae88e5e45d5849179ec38e
alexa-brave <merkel_795@mail.ru>
2026-04-18 23:52:07 +0300

    fix: вывод в не в stderr, а в stdout

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,4 +5,4 @@ from ..find_diff import generate_diff
 def main():
     args = files_parser()
     result = generate_diff(args.first_file, args.second_file, args.format)
-    return result
+    print(result)
